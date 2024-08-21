3c47d7ea5e522d4b2f35a8b969a661bd02fefd8d
Matthew Thomas <matthewthomasgb@gmail.com>
2024-08-21 20:15:28 +0100

    chore: add new line

--- src/summary.ts
+++ src/summary.ts
@@ -35,9 +35,8 @@ export function generateTestDetailsTable(tests: CtrfTest[]): void {
 
             if (tests.length > maxRows) {
                 limitedTests = tests.slice(0, maxRows);
-                core.summary.addRaw(`Note: You have a lot of tests. We've limited the number shown in the detailed breakdown to ${maxRows}.`);
+                core.summary.addRaw(`Note: You have a lot of tests. We've limited the number shown in the detailed breakdown to ${maxRows}.\n`);
             }
-            core.summary.addEOL();
             core.summary.addLink('A ctrf plugin', 'https://github.com/ctrf-io/github-actions-ctrf');
 
     } catch (error) {
