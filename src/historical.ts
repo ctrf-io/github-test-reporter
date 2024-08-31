d5eafe75f8ba9644bc1308047522cfc2eeb57626
Matthew Thomas <matthewthomasgb@gmail.com>
2024-08-31 20:54:27 +0100

    chore: add initial historical

---
+++ src/historical.ts
@@ -0,0 +1,33 @@
+// import https from 'https';
+import { CtrfReport } from '../types/ctrf';
+// import * as core from '@actions/core';
+import fs from 'fs';
+
+
+export function generateHistoricSummary(report: CtrfReport): void {
+    const token = process.env.GITHUB_TOKEN;
+    if (!token) {
+        console.error('GITHUB_TOKEN is not set. This is required for post-comment argument');
+        return;
+    }
+
+    const eventPath = process.env.GITHUB_EVENT_PATH;
+    if (!eventPath) {
+        console.error('GITHUB_EVENT_PATH is not set. This is required to determine context.');
+        return;
+    }
+
+    let context;
+    try {
+        const eventData = fs.readFileSync(eventPath, 'utf8');
+        context = JSON.parse(eventData);
+    } catch (error) {
+        console.error('Failed to read or parse event data:', error);
+        return;
+    }
+
+    const repo = context.repository.full_name;
+
+    console.log('GitHub Event Context:', JSON.stringify(context, null, 2));
+
+}
