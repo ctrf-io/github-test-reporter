82647ec6281b25f0596a374bc44985caa41e78ff
Matthew Thomas <matthewthomasgb@gmail.com>
2024-12-08 12:34:29 +0000

    fix: path to templates

--- src/reports/core.ts
+++ src/reports/core.ts
@@ -1,18 +1,18 @@
 import { join } from 'path';
 
 export const BuiltInReports = {
-  SummaryTable: join(__dirname, 'reports', 'summary-table.hbs'),
-  TestTable: join(__dirname, 'reports', 'test-table.hbs'),
-  TestList: join(__dirname, 'reports', 'test-list.hbs'),
-  FailedTable: join(__dirname, 'reports', 'failed-table.hbs'),
-  FailedFolded: join(__dirname, 'reports', 'failed-folded.hbs'),
-  FailRateTable: join(__dirname, 'reports', 'fail-rate-table.hbs'),
-  SkippedTable: join(__dirname, 'reports', 'skipped-table.hbs'),
-  FlakyTable: join(__dirname, 'reports', 'flaky-table.hbs'),
-  FlakyRateTable: join(__dirname, 'reports', 'flaky-rate-table.hbs'),
-  AiTable: join(__dirname, 'reports', 'ai-table.hbs'),
-  PreviousResultsTable: join(__dirname, 'reports', 'previous-results-table.hbs'),
-  PullRequest: join(__dirname, 'reports', 'pull-request.hbs'),
-  SuiteFolded: join(__dirname, 'reports', 'suite-folded.hbs'),
-  SuiteList: join(__dirname, 'reports', 'suite-list.hbs')
+  SummaryTable: join(__dirname, 'summary-table.hbs'),
+  TestTable: join(__dirname, 'test-table.hbs'),
+  TestList: join(__dirname, 'test-list.hbs'),
+  FailedTable: join(__dirname, 'failed-table.hbs'),
+  FailedFolded: join(__dirname, 'failed-folded.hbs'),
+  FailRateTable: join(__dirname, 'fail-rate-table.hbs'),
+  SkippedTable: join(__dirname, 'skipped-table.hbs'),
+  FlakyTable: join(__dirname, 'flaky-table.hbs'),
+  FlakyRateTable: join(__dirname, 'flaky-rate-table.hbs'),
+  AiTable: join(__dirname, 'ai-table.hbs'),
+  PreviousResultsTable: join(__dirname, 'previous-results-table.hbs'),
+  PullRequest: join(__dirname, 'pull-request.hbs'),
+  SuiteFolded: join(__dirname, 'suite-folded.hbs'),
+  SuiteList: join(__dirname, 'suite-list.hbs')
 } as const;
