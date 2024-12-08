7dcf19cdab62d45bf1845cc164a3175e311ccc9e
Matthew Thomas <matthewthomasgb@gmail.com>
2024-12-08 12:06:05 +0000

    feat: add paths for publish

--- src/reports/core.ts
+++ src/reports/core.ts
@@ -1,16 +1,18 @@
-export enum BuiltInReports {
-  SummaryTable = 'src/reports/summary-table.hbs',
-  TestTable = 'src/reports/test-table.hbs',
-  TestList = 'src/reports/test-list.hbs',
-  FailedTable = 'src/reports/failed-table.hbs',
-  FailedFolded = 'src/reports/failed-folded.hbs',
-  FailRateTable = 'src/reports/fail-rate-table.hbs',
-  SkippedTable = 'src/reports/skipped-table.hbs',
-  FlakyTable = 'src/reports/flaky-table.hbs',
-  FlakyRateTable = 'src/reports/flaky-rate-table.hbs',
-  AiTable = 'src/reports/ai-table.hbs',
-  PreviousResultsTable = 'src/reports/previous-results-table.hbs',
-  PullRequest = 'src/reports/pull-request.hbs',
-  SuiteFolded = 'src/reports/suite-folded.hbs',
-  SuiteList = 'src/reports/suite-list.hbs',
-}
+import { join } from 'path';
+
+export const BuiltInReports = {
+  SummaryTable: join(__dirname, 'reports', 'summary-table.hbs'),
+  TestTable: join(__dirname, 'reports', 'test-table.hbs'),
+  TestList: join(__dirname, 'reports', 'test-list.hbs'),
+  FailedTable: join(__dirname, 'reports', 'failed-table.hbs'),
+  FailedFolded: join(__dirname, 'reports', 'failed-folded.hbs'),
+  FailRateTable: join(__dirname, 'reports', 'fail-rate-table.hbs'),
+  SkippedTable: join(__dirname, 'reports', 'skipped-table.hbs'),
+  FlakyTable: join(__dirname, 'reports', 'flaky-table.hbs'),
+  FlakyRateTable: join(__dirname, 'reports', 'flaky-rate-table.hbs'),
+  AiTable: join(__dirname, 'reports', 'ai-table.hbs'),
+  PreviousResultsTable: join(__dirname, 'reports', 'previous-results-table.hbs'),
+  PullRequest: join(__dirname, 'reports', 'pull-request.hbs'),
+  SuiteFolded: join(__dirname, 'reports', 'suite-folded.hbs'),
+  SuiteList: join(__dirname, 'reports', 'suite-list.hbs')
+} as const;
