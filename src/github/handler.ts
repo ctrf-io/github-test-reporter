d346b89dc0c8281db3d328bc1d83339aecd516eb
Matthew Thomas <matthewthomasgb@gmail.com>
2024-12-17 18:28:03 +0000

    fix: lint issues

--- src/github/handler.ts
+++ src/github/handler.ts
@@ -29,9 +29,9 @@ export async function handleViewsAndComments(
   generateViews(inputs, report)
 
   if (shouldAddCommentToPullRequest(inputs, report)) {
-    const INVISIBLE_MARKER = inputs.commentTag ? 
-      `<!-- CTRF PR COMMENT TAG: ${inputs.commentTag} -->` :
-      `<!-- CTRF PR COMMENT TAG: DEFAULT -->`
+    const INVISIBLE_MARKER = inputs.commentTag
+      ? `<!-- CTRF PR COMMENT TAG: ${inputs.commentTag} -->`
+      : `<!-- CTRF PR COMMENT TAG: DEFAULT -->`
 
     await postOrUpdatePRComment(inputs, INVISIBLE_MARKER)
   }
