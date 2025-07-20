1eaf804eedbd06d8ba900f7044df2a78f052f8af
Matthew Thomas <matthewthomasgb@gmail.com>
2025-07-20 12:19:34 +0100

    chore: add formatDecimalRate helper for percentage formatting and update fail-rate-table template

--- src/handlebars/helpers/index.ts
+++ src/handlebars/helpers/index.ts
@@ -14,6 +14,7 @@ import {
   formatDurationMsToHumanHelper,
   formatDurationStartStopToHumanHelper,
   formatRateHelper,
+  formatDecimalRateHelper,
   formatTestPathHelper,
   getCollapseLargeReportsHelper,
   getEmojiHelper,
@@ -48,6 +49,7 @@ export function registerAllHelpers(): void {
   moreThanHelper()
   sortTestsByFlakyRateHelper()
   formatRateHelper()
+  formatDecimalRateHelper()
   sortTestsByFailRateHelper()
   sliceArrayHelper()
   reverseArray()
