424a654661cbb7ccb4089e2f8d60a79bfd811d2a
Matthew Thomas <matthewthomasgb@gmail.com>
2026-05-03 10:55:30 +0100

    fix: set platform=node in tsup config to resolve Dynamic require error

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: { index: 'src/index.ts' },
   format: ['esm'],
+  platform: 'node',
   outDir: 'dist',
   sourcemap: true,
   splitting: false,
